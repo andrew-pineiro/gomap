774ee61fd5a579a176328c0731ac833c5b73d01d
andrew-pineiro <apineiro@towerpower.com>
2024-03-04 21:54:05 -0500

    working stages, also closes #2

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/netip"
 	"os"
@@ -11,7 +12,7 @@ import (
 
 type Arguments struct {
 	IPAddress string
-	Mask      net.IPMask
+	Mask      string
 	Ports     []string
 }
 type ValidIPs struct {
@@ -19,20 +20,24 @@ type ValidIPs struct {
 	Port      string
 }
 
-func verifyIP(ip string) bool {
-	_, _, err := net.ParseCIDR(ip)
-	if err != nil {
-		_, err := netip.ParseAddr(ip)
+func verifyIP(ip string) (string, string) {
+
+	if strings.Contains(ip, "/") {
+		_, ipnet, err := net.ParseCIDR(ip)
 		if err != nil {
-			//fmt.Printf("ERROR: %s is not a valid ip address\n", args[1])
-			return false
+			log.Fatalf("ERROR: not a valid IP address %s", ip)
 		}
+		return ipnet.IP.String(), ipnet.Mask.String()
+	} else {
+		ipnet, err := netip.ParseAddr(ip)
+		if err != nil {
+			log.Fatalf("ERROR: not a valid IP address %s", ip)
+		}
+		return ipnet.String(), ""
 	}
-	return true
 }
 
 func testIPAddress(ip string, port string) bool {
-	//TODO(#1): add subnet mask usage
 	//TODO(#2): handle not found ip addresses
 	timeout := time.Millisecond * 10
 	conn, err := net.DialTimeout("tcp", ip+":"+port, timeout)
@@ -44,18 +49,17 @@ func testIPAddress(ip string, port string) bool {
 }
 
 func getArgs(args []string) Arguments {
-	var a Arguments
-	if verifyIP(args[1]) {
-		a.IPAddress = args[1]
-		if len(args) > 2 {
-			if strings.Contains(args[2], "-") {
-				a.Ports = strings.Split(args[2], "-")
-			} else {
-				a.Ports = append(a.Ports, args[2])
-			}
+	ip, mask := verifyIP(args[1])
+	a := Arguments{
+		IPAddress: ip,
+		Mask:      mask,
+	}
+	if len(args) > 2 {
+		if strings.Contains(args[2], "-") {
+			a.Ports = strings.Split(args[2], "-")
+		} else {
+			a.Ports = append(a.Ports, args[2])
 		}
-	} else {
-		fmt.Printf("ERROR: %s is not a valid IP address", args[1])
 	}
 	return a
 }
@@ -66,13 +70,18 @@ func main() {
 	if args.IPAddress != "" {
 		var ips []ValidIPs
 		for _, port := range args.Ports {
-			valid := testIPAddress(args.IPAddress, port)
-			if valid {
-				validIP := ValidIPs{
-					IPAddress: args.IPAddress,
-					Port:      port,
+			if args.Mask != "" {
+				//TODO(#1): add subnet mask usage
+				log.Fatalf("subnet mask usage not implemented")
+			} else {
+				valid := testIPAddress(args.IPAddress, port)
+				if valid {
+					validIP := ValidIPs{
+						IPAddress: args.IPAddress,
+						Port:      port,
+					}
+					ips = append(ips, validIP)
 				}
-				ips = append(ips, validIP)
 			}
 		}
 		if len(ips) == 0 {
